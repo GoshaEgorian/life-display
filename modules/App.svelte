e6dd7fd3a29f6ca71cb1ca959b89045826a23941
George Egorian <gosha.egorian@gmail.com>
2019-12-08 22:06:57 +0300

    Add calendar displaying

--- modules/App.svelte
+++ modules/App.svelte
@@ -1,9 +1,23 @@
 <script>
   import Week from './Week.svelte';
+
+  const weeksAmount = 59;
+  const currentWeek = 31;
 </script>
 
-<Week done/>
+<section class={"calendar"}>
+  {#each Array(weeksAmount) as _, i}
+    <Week done={i+1 <= currentWeek} />
+  {/each}
+</section>
 
 <style>
   @import "../node_modules/normalize.css";
+
+  .calendar {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
