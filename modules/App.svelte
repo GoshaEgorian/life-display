b47d69d8b5302a8af811b804143a5991de37d450
George Egorian <gosha.egorian@gmail.com>
2019-12-08 21:38:42 +0300

    Add week component

---
+++ modules/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Week from './Week.svelte';
+</script>
+
+<Week done/>
+
+<style>
+  @import "../node_modules/normalize.css";
+</style>
---
+++ modules/Week.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let done = false;
+</script>
+
+<div class={"week"} class:done></div>
+
+<style>
+  .week {
+    width: 15px;
+    height: 15px;
+    border: 1px soled grey;
+  }
+  .done {
+    background-color: grey;
+  }
+</style>
